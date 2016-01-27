e0b3f754a33c31277928d8426ee798e0178c058a
Beto23 <josee-45@hotmail.com>
2016-01-26 18:43:22 -0600

    install accordion

--- src/index.js
+++ src/index.js
@@ -8,19 +8,21 @@ require('angular-aria/angular-aria.js')
 require('angular-animate/angular-animate.js')
 require('angular-material/angular-material.js')
 require('ngmap');
+require('v-accordion/dist/v-accordion.js');
+
 
 var jQuery = $ = require('jquery');
 require('normalize.css/normalize.css');
 require('reset.css/reset.css');
 require('angular-timeline/dist/angular-timeline.css');
 require('angular-carousel/dist/angular-carousel.css')
-
+require('v-accordion/dist/v-accordion.css');
 require('angular-material/angular-material.css')
 
 require('./app.styl');
 
 /* App */
-var app = angular.module('ITS', ['ui.router','Header','Helpers','App','ngMaterial','ngMap']);
+var app = angular.module('ITS', ['ui.router','Header','Helpers','App','ngMaterial','ngMap', 'vAccordion']);
 
 require('./app.config.js')(app);
 require('./app/')();
