63498b5a39adf326de39698c8ad2c56730c75b12
Beto23 <josee-45@hotmail.com>
2016-01-27 01:46:10 -0600

    Add list tecnologicos

--- src/app/documentacion/documentacion.controller.js
+++ src/app/documentacion/documentacion.controller.js
@@ -1,9 +1,91 @@
 module.exports = function(ngModule){
 	ngModule.controller('documentacionCtrl', documentacionCtrl);
 
-	documentacionCtrl.$inject = ['HelpersFactory']
-	function documentacionCtrl(HelpersFactory){
+	documentacionCtrl.$inject = ['HelpersFactory', '$scope']
+	function documentacionCtrl(HelpersFactory, $scope){
 		HelpersFactory.bgFullPage();
+
+		var logoSaltillo = './img/logoITS.png';
+		    $scope.delegaciones = [
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      },
+		      {
+		        logoTec : logoSaltillo,
+		        tec: 'IT Saltillo',
+		        programacion: 'Programacion',
+		        municipio: 'Saltillo'
+		      }
+		    ];
 	};
 
 
