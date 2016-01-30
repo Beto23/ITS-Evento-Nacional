888581ad48c69377ecab0f584078628557ad1154
Edgar Zavala <ezavile@gmail.com>
2016-01-30 01:48:42 -0600

    ng-repeat multimedia

--- src/app/multimedia/multimedia.controller.js
+++ src/app/multimedia/multimedia.controller.js
@@ -3,12 +3,40 @@ module.exports = function(ngModule){
 
 	function MultimediaCtrl(){
 		var multimedia = this;
-		multimedia.imgs = ['./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg']
 		multimedia.isPopupOn = false;
+		multimedia.eventoSeleccionado = [];
 		$('.Multimedia-type').click(function(){
 			var op = $(this).parent().attr('id');
 			$('.'+op).toggleClass('isOpen')
 			$(this).toggleClass('isOpen')
 		})
+
+		multimedia.galeria = {}
+		multimedia.galeria.eventos = [
+			{
+				nombre: 'Inaguración',
+				fecha: '10 de febrero',
+				img: './img/1.jpg',
+				galeria: ['./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg','./img/1.jpg','./img/2.jpg']
+			},
+			{
+				nombre: 'Inaguración2',
+				fecha: '10 de febrero',
+				img: './img/1.jpg',
+				galeria: ['./img/1.jpg','./img/2.jpg']
+			},
+			{
+				nombre: 'Inaguración3',
+				fecha: '10 de febrero',
+				img: './img/1.jpg',
+				galeria: ['./img/1.jpg']
+			}
+		]
+
+
+		multimedia.seleccionarEvento = function(galeria){
+			multimedia.isPopupOn = true;
+			multimedia.eventoSeleccionado = galeria;
+		}
 	}
 };
