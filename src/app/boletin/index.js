6bc3e2e6666704de6497fb8585352ec26f9d3a0e
Beto23 <josee-45@hotmail.com>
2016-02-03 16:09:41 -0600

    add acentos

--- src/app/boletin/index.js
+++ src/app/boletin/index.js
@@ -3,7 +3,7 @@ module.exports = function(ngModule){
 	require('./boletin.controller.js')(ngModule);
 	require('./boletin.directive.js')(ngModule);
 	require('./boletin1/')(ngModule);
-
+	require('./boletin2/')(ngModule);
 
 
 }
