d1a1cd448b7b708700ad121c681715326edd43f5
Beto23 <josee-45@hotmail.com>
2016-02-17 19:25:11 -0600

    add tel de emergencia

--- src/app/more/telefonos/telefono.controller.js
+++ src/app/more/telefonos/telefono.controller.js
@@ -17,7 +17,7 @@ module.exports = function(ngModule){
 				maps: 'https://www.google.com.mx/maps/place/Hospital+Universitario+de+Saltillo/@25.4274937,-101.016436,17z/data=!3m1!4b1!4m2!3m1!1s0x8688729ad6a0b2e5:0x1eddf4a7ab0b69b5'
 			},
 			{
-				lugar : 'Hospital la Concepción',
+				lugar : 'Hospital la Concepci&#243;n',
 				telefono : '(844) 4161 022  / 4161082  / 4155917',
 				img: 'http://www.cmh.mx/imagenes/asociados/img26.jpg',
 				maps: 'https://www.google.com.mx/maps/place/La+Concepci%C3%B3n/@25.4615707,-100.983377,17z/data=!4m5!1m2!2m1!1sHospital+la+Concepci%C3%B3n!3m1!1s0x86886d4b8abb8f31:0x5d3d3fb94fe47a29'
@@ -27,6 +27,30 @@ module.exports = function(ngModule){
 				telefono : '(844) 411 7000 al 09 / 411 7047',
 				img: 'https://pbs.twimg.com/profile_images/529418330986127362/MTGJPGJo_400x400.png',
 				maps: 'https://www.google.com.mx/maps/place/Christus+Muguerza+Hospital+Saltillo/@25.4616133,-100.9987463,14z/data=!4m5!1m2!2m1!1sHOSPITAL+CHRISTUS+MUGUERZA!3m1!1s0x86882202bbbb20bd:0x803240b2ca922fc7'
+			},
+			{
+				lugar : 'Polic&#237;a y Tr&#225;nsito',
+				telefono : '066 / (844) 414 1616',
+				img: 'https://pbs.twimg.com/profile_images/2404256820/cid_62E0FE9C-DA6D-44E6-A089-07528E0D08ED.jpg',
+				maps: 'https://www.google.com.mx/maps/place/Direcci%C3%B3n+de+Policia+Preventiva+Municipal/@25.415586,-100.976748,15z/data=!4m2!3m1!1s0x0:0x31db855bd22aaf3e'
+			},
+			{
+				lugar : 'Cruz roja',
+				telefono : '(065 / (844) 4148140 / 4143333 / 4148335',
+				img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_the_Red_Cross.svg/150px-Flag_of_the_Red_Cross.svg.png',
+				maps: 'https://www.google.com.mx/maps/place/Esc.+Ma.+Josefa+Medina+de+Valle+Arizpe+Cruz+Roja+Mexicana/@25.442594,-101.0183231,13z/data=!4m5!1m2!2m1!1scruz+roja!3m1!1s0x868872a56e0ca305:0x46e4bfc6bd8b294'
+			},
+			{
+				lugar : '&#225;ngeles Verdes',
+				telefono : '078 / (844) 412 4050',
+				img: 'http://www.periodicoenfoque.com.mx/wp-content/uploads/2013/08/%C3%81ngeles-Verdes.jpg',
+				maps: 'https://www.google.com.mx/maps/place/Corporaci%C3%B3n+de+Servicios+al+Turista+%C3%81ngeles+Verdes/@25.4332049,-100.9998569,17z/data=!3m1!4b1!4m2!3m1!1s0x868872a14b3321ab:0x24b5b5439c538ddc'
+			},
+			{
+				lugar : 'Polic&#237;a Estatal',
+				telefono : '066  / (844) 434 0945 /434 0810 /434 0450',
+				img: 'http://201.144.124.181:8080/ces/NUEVA/wp-content/uploads/2013/10/POLICIA-ESTATAL-2.jpg',
+				maps: 'https://www.google.com.mx/maps/place/Comisi%C3%B3n+Estatal+de+Seguridad/@25.4121638,-100.9779788,17z/data=!3m1!4b1!4m2!3m1!1s0x86880d342515ccab:0xa9e861a95a55eb78'
 			}
 		]
 	}
