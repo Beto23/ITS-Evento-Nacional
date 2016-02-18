7dbc9bf3f133f74f555d5ae727d16f7c93d411e7
Beto23 <josee-45@hotmail.com>
2016-02-17 23:36:01 -0600

    rename url more to lugares

--- src/app.config.js
+++ src/app.config.js
@@ -29,7 +29,7 @@ module.exports = function(ngApp) {
 				controller: 'BoletinCtrl'
 			})
 			.state('more',{
-				url: '/moreInfo',
+				url: '/lugares',
 				template: require('./app/more/more.html'),
 				controllerAs: 'more',
 				controller: 'MoreCtrl'
